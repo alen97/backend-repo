7daec5a1ab549901d46c17dfbcbce013eeb4305f
gabrielg97 <gabrielg@epidataconsulting.com>
2022-10-26 12:09:53 -0300

    fix

--- src/main/java/com/proyecto/integrador/Security/Enums/RolNombre.java
+++
@@ -1,9 +0,0 @@
-
-package com.proyecto.integrador.Security.Enums;
-
-
-public enum RolNombre {
-    ROLE_ADMIN, ROLE_USER;
-    //User: ve los productos
-    //Admin: los ve, crea y edita los productos
-}
